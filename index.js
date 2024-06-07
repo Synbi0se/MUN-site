408fe08426dea3d5a2540f54dbc8a49bc875c689
Synbiose <123572921+Synbi0se@users.noreply.github.com>
2024-06-07 12:18:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const languageButton = document.getElementById("languageButton");
+    const languageButtonImage = document.getElementById("languageButtonImage");
+
+    // Button images for different languages
+    const buttonImages = {
+        en: "img/US flag.png",
+        fr: "img/Fr flag.png"
+    };
+
+    // Default language
+    let currentLanguage = 'en';
+
+    // Function to change language image
+    const changeLanguageImage = () => {
+        currentLanguage = currentLanguage === 'en' ? 'fr' : 'en';
+        languageButtonImage.src = buttonImages[currentLanguage];
+        languageButtonImage.alt = currentLanguage === 'en' ? 'English' : 'French';
+    };
+
+    // Add event listener to button
+    languageButton.addEventListener("click", changeLanguageImage);
+});

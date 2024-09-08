a25f5e2aab374bd093e38a283e35ef10b94a8e5a
virgilefsy <virgile.foissey@gmail.com>
2024-09-08 11:50:14 +0200

    save from 08/09
    Download works

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ function toggleMenu() {
     }
 }
 
-function downloadDocuments() {
-    window.open("https://docs.google.com/forms/d/e/1FAIpQLSfYGK3mRNtdHU3td72VgmclpRCcuZYbGdaNJpmpu32LjDAdIQ/viewform?usp=sf_link")
+function downloadDocuments(nom) {
+    const lien = document.createElement('a');
+    lien.href = `doc/${nom}`;
+    lien.download = nom;
+    lien.click();
 }
-
+function registerMun(lng){
+  if (lng === "EN") {
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSfYGK3mRNtdHU3td72VgmclpRCcuZYbGdaNJpmpu32LjDAdIQ/viewform?usp=sf_link");
+    
+  } else {
+    console.log("merde");
+  }
+}
 
 
 // document.addEventListener("DOMContentLoaded", function() {

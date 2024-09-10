513112f922e800f9ab3aa75348761bb7792d1b20
Synbiose <t.thomas.semence@gmail.com>
2024-09-10 17:01:16 +0200

    Save from 10/09
    flm

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ function toggleMenu() {
 }
 
 function downloadDocuments(nom) {
-    const lien = document.createElement('a');
-    lien.href = `https://synbi0se.github.io/MUN-site/doc/${nom}`;
-    lien.download = `https://synbi0se.github.io/MUN-site/doc/${nom}`;
-    document.body.appendChild(lien);
-    lien.click();
-    document.body.removeChild(lien);
+  const lien = document.createElement('a');
+  lien.href = `https://synbi0se.github.io/MUN-site/doc/${nom}`;
+  lien.download = `https://synbi0se.github.io/MUN-site/doc/${nom}`;
+  document.body.appendChild(lien);
+  lien.click();
+  document.body.removeChild(lien);
 }
 function registerMun(lng){
   if (lng === "EN") {
@@ -26,17 +26,25 @@ function registerMun(lng){
 }
 
 function invitation(language) {
-  const box = document.getElementById('frame')  
- if (language === 'fr') {
-  box.innerHTML = '<iframe src="doc/Invitation to Chaumun 2025.pdf" frameborder="0"></iframe>' ;
- }
+  const box = document.getElementById('frame');
+  const en = document.getElementById('en');
+  const fr = document.getElementById('fr');
+  const de = document.getElementById('de');
+  en.style.backgroundColor = '#007D45';
+  fr.style.backgroundColor = '#007D45';
+  de.style.backgroundColor = '#007D45';
  if (language === 'en') {
+  box.innerHTML = '<iframe src="doc/Invitation to Chaumun 2025.pdf" frameborder="0"></iframe>' ;
+  en.style.backgroundColor = '#004828';
+  }
+ if (language === 'fr') {
   box.innerHTML = '<iframe src="doc/Invitation au Chaumun 2025.pdf" frameborder="0"></iframe>' ;
- }
+  fr.style.backgroundColor = '#004828';
+  }
  if (language === 'de') {
   box.innerHTML = '<iframe src="doc/Einladung zum Chaumun.pdf" frameborder="0"></iframe>' ;
- }
-
+  de.style.backgroundColor = '#004828';
+  }
 }
 
 

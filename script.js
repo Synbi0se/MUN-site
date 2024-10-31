3867863f8a749b51839c5b1ea255e9408e919742
Synbiose <t.thomas.semence@gmail.com>
2024-10-31 18:21:19 +0100

    Adding semi colons in JS...

--- script.js
+++ script.js
@@ -10,28 +10,28 @@ function toggleMenu() {
 
 // function to open the documents in ..-docs.html 
 function openDocs(name){
-  window.open(name)
+  window.open(name);
 }
 
 // function to register 
 function registerMun(lng){
   if (lng === 'en') {
-  	alert("Currently unavailable, contact us at chaumun.lcdg@gmail.com")
+  	alert("Currently unavailable, contact us at chaumun.lcdg@gmail.com");
   }
   else { // aka fr
-  	alert("Insdisponible actuellement, contactez nous à chaumun.lcdg@gmail.com")
+  	alert("Insdisponible actuellement, contactez nous à chaumun.lcdg@gmail.com");
   }
 }
 
 function giveMeTheDocs(lng){
   if (lng=='en') {
-    window.open('doc/ressources/en/Legal-Dimensions-of-Sea-Level-Rise-Pacific-Perspectives.pdf')
-    window.open('doc/ressources/en/The-Impact-of-Sea-Level-Rise-on-Developing-Countries.pdf')
-    window.open('doc/ressources/en/The-rate-of-global-sea-level-rise-doubled.pdf')
-    window.open('doc/ressources/en/WB-Kiribati-Country-Profile.pdf')
-    window.open('doc/ressources/en/Bibliography-sitography.docx')
+    window.open('doc/ressources/en/Legal-Dimensions-of-Sea-Level-Rise-Pacific-Perspectives.pdf');
+    window.open('doc/ressources/en/The-Impact-of-Sea-Level-Rise-on-Developing-Countries.pdf');
+    window.open('doc/ressources/en/The-rate-of-global-sea-level-rise-doubled.pdf');
+    window.open('doc/ressources/en/WB-Kiribati-Country-Profile.pdf');
+    window.open('doc/ressources/en/Bibliography-sitography.docx');
   }
   else{
-    alert('Rien pour le moment... Nothing for now...')
+    alert('Rien pour le moment... Nothing for now...');
   }
 }

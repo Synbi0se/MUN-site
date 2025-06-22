df5f82688daf9fee65136cd669eb26e695c4f08d
Synbiose <t.thomas.semence@gmail.com>
2025-06-22 22:35:13 +0200

    Struggling with my fkng menu

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// function Clear(){
+//     let b1 = document.getElementById('btn-openus');
+//     let b2 = document.getElementById('btn-openinfos');
+//     b1.style.background = '#00000000';
+//     b2.style.background = '#00000000';
+// }
 function OpenUs(){
-    console.log("Open Us");
+    console.log('Open Us'); //à retirer
+    
 }
 function OpenInfos(){
-    console.log("Open Infos");
+    console.log('Open Infos'); //à retirer
+    // ...
 }

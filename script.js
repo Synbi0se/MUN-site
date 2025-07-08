c479488fe395f8ea82d699921584a85f1bedd2fb
Synbiose <t.thomas.semence@gmail.com>
2025-07-08 20:45:42 +0200

    3.9
    added sponsors to archive
    added sponsors via js, fantastic
    added color to school
    added melina to team
    changed image sizes for team to be 1k.1k
    added calendar in home

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     if (window.location.pathname.endsWith('home.html')) {
-//         const SFile = [
-//            's.webp',
-           
-//         ]
-//         const SDiv = document.getElementById('sponsors');
-//         SFile.forEach(file => {
-//             SDiv.innerHTML += `<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 sponsors-element"><img src="../img/sponsors/${file}" alt=""></div>`;
-//             // Log the files to the console
-//             console.log('Sponsor files:', SFile);
-//         })
-//     }
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    let N = 12;
+    // First sponsors section : Randomly selected
+    let Sdiv1 = document.getElementById('s1');
+    let Lst = [];
+    if (Sdiv1 !== null) {
+        while (Lst.length < 6){
+            let Random = Math.floor(Math.random() * N) + 1;
+            if (!Lst.includes(Random)) {
+                Lst.push(Random);
+            }
+            Lst.sort((a, b) => a - b);
+        }
+        for(let i = 0; i<6; i++) {
+        Sdiv1.innerHTML += `<div class="col-sm-4 col-lg-2 sponsors-element"><img src="../img/sponsors/s(${Lst[i]}).webp" alt=""></div>`;
+        }
+    }
+    
+    // Second sponsors section : All sponsors
+    if (window.location.pathname.endsWith('home.html')) {
+        let Sdiv2 = document.getElementById('s2');
+        for (i = 0; i < N; i++) {
+            Sdiv2.innerHTML += `<div class="col-sm-4 col-lg-2 sponsors-element"><img src="../img/sponsors/s(${i+1}).webp" alt=""></div>`;
+        }
+    }
+});
+
 function OpenDocs(doc) {
     console.log(doc)
 }

ed4c6902e6dcb91378103ff7ba2afbca0c53c581
T BRIL <123572921+Synbi0se@users.noreply.github.com>
2026-02-23 18:12:10 +0100

    3.23
    Using Supabase now

--- rcs/script.js
+++ rcs/script.js
@@ -27,28 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
             Sdiv2.innerHTML += `<div class="col-sm-4 col-lg-2 sponsors-element"><img src="../img/sponsors/s(${i+1}).webp" alt=""></div>`;
         }
     }
-
-    // Fetching infos from attrib and put them in the 3 lists   Only in account.html
-    if(window.location.pathname.endsWith('account.html')){
-      fetch('../rcs/attrib.csv')
-      .then(response => response.text())
-      .then(text => {
-        const rows = text.trim().split('\n');
-        const dataRows = rows.slice(1);
-        dataRows.forEach(row => {
-          const [mail, committee, attrib] = row.split(',');
-          if (mail) {
-            mailList.push(mail);
-          }
-          if (committee) {
-            committeeList.push(committee);
-          }
-          if (attrib) {
-            attribList.push(attrib);
-          }
-        });
-      });
-    }
 });
 
 function openDocs(doc) {
@@ -56,20 +34,4 @@ function openDocs(doc) {
 }
 function error(message) {
   alert(message)
-}
-
-// When activated by a button, shows the infos of a given email (if found in the datas)
-function showDelegateAttribution() {
-  const mailbox = document.getElementById('signin-mail');
-  const mail = mailbox.value
-  const attrib = document.getElementById('attrib');
-  const index = mailList.indexOf(mail);
-  if (mailList.includes(mail)) {
-    attrib.innerHTML = `Attribution : ${attribList[index]} (${committeeList[index]})`
-    mailbox.style.border = 'solid 1px #00c400ff'
-  }
-  else{
-    attrib.innerHTML = 'Attribution not settled / Attibution non faite'
-    mailbox.style.border = 'solid 1px #9C2007'
-  }
 }
